81c7dc7016e87ab4345dfe820f751954dc853983
brunomestres <bruno.mestres@poscontrole.com.br>
2022-06-26 23:39:15 -0300

    Button test

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -1,3 +1,20 @@
-test("sum", () => {
-  expect(1 + 1).toBe(2);
-});
+import { render, screen } from "@testing-library/react";
+import userEvent from '@testing-library/user-event';
+import App from "./App";
+
+
+describe("App component" , () => {
+  it("should be render list" , () => {
+    render(<App />);
+    expect(screen.getByText("Bruno")).toBeInTheDocument();
+    expect(screen.getByText("Fernando")).toBeInTheDocument();
+    expect(screen.getByText("Tchalla")).toBeInTheDocument();
+  });
+
+  it("button", () => {
+    render(<App />);
+    const addButton = screen.getByText("Adicionar");
+    userEvent.click(addButton);
+    expect(screen.getByText("Novo")).toBeInTheDocument();
+  })
+})
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState} from 'react';
 
 function App() {
+  const [item, setItem] =useState(['Bruno', 'Fernando', 'Tchalla']);
+
+  const addItem = () => {
+    setItem((oldValue) => [...oldValue, "Novo"]);
+  }
+
   return (
     <div >
      
+     <button onClick={addItem}>Adicionar</button>
+      <ul>
+        {item.map( e => <li key={e}>{e}</li>)}
+      </ul>
     </div>
   );
 }
